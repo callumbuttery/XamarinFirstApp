30281255a99ba2a6b1c27e17375f34071a54cf9d
callumbuttery <46718195+callumbuttery@users.noreply.github.com>
2020-06-13 00:10:42 +0100

    Adding basic user input and screen UI update with variable

---
+++ FirstApp/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ButtonClicked(object sender, EventArgs e)
+        {
+
+            string text = Entry.Text; //cast sender to access the properties of the Entry
+
+            //update value of label to value inputed by user
+            Label.Text = text;
+        }
+    }
+}
